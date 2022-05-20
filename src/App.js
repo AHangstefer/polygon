3215952e26bb1cc38e38bb8d2d7af92661f37a6f
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2022-05-20 18:11:07 -0400

    got data, need to do calculations and decide what's rendered

--- src/App.js
+++ src/App.js
@@ -12,21 +12,41 @@ function App() {
 
   useEffect(()=> {
     axios
-    .get(`https://api.polygon.io/v2/ticks/stocks/trades/AAPL/2020-10-20?reverse=true&limit=10&apiKey=${key}`)
+    .get(`https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=${key}`)
     .then(res => {
       console.log("this is res from app.js polygon get", res)
-      stockSet(res)
+      stockSet(res.data)
     })
     .catch(err => {
       console.log("Oh no! WHERE'S THE DATA?!", err)
     })
-  })
+  },[])
+
+  let close = stock.close;
+  let open = stock.open;
+  let high = stock.high;
+  let low = stock.low;
+
+  console.log("close, open, high, low", close, open, high, low)
+
+
 
   return (
-    <div className="App"> Hello App
+    <>
+    <div className="App"> 
+      <p></p>
+    
     
     </div>
+    </>
   );
 }
 
 export default App;
+
+
+// {Object.values(stock).forEach(items=>
+//   Object.values(items).map(item => (
+//     console.log("items", item)
+//     ))
+//    )}
