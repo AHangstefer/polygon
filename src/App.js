cd5066dfca0b2abdd55da931b3a0b6820bbcd8e1
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2022-05-20 15:36:55 -0400

    getting started, hit api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import './App.css';
 
+
+
 function App() {
+
+  const [stock, stockSet] = useState([])
+
+  const key = process.env.REACT_APP_API_KEY
+
+  useEffect(()=> {
+    axios
+    .get(`https://api.polygon.io/v2/ticks/stocks/trades/AAPL/2020-10-20?reverse=true&limit=10&apiKey=${key}`)
+    .then(res => {
+      console.log("this is res from app.js polygon get", res)
+      stockSet(res)
+    })
+    .catch(err => {
+      console.log("Oh no! WHERE'S THE DATA?!", err)
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> Hello App
+    
     </div>
   );
 }
