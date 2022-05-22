9ea9a47b6841d01beb3693212acc5d27179a54c5
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2022-05-22 15:32:55 -0400

    added math and all data

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   let [ticker, setTicker] = useState("")
   let [date, setDate] = useState("");
   let [stock, setStock] =useState([])
+
+  let open = stock.open;
+  let close = stock.close;
+  let difference = (close - open).toFixed(2)
+  let percentChange = ((difference/open) *100).toFixed(2)
   
 
   const key = process.env.REACT_APP_API_KEY
@@ -39,45 +44,52 @@ function App() {
 
   return (
     <>
-    <div className="App"> Find Your Stock
-      <div className = "inputs and button">
-        <input
-          type = "text"
-          value = {ticker}
-          onChange={(e)=> setTicker(e.target.value)}
-          placeholder = "ticker"
-          >
-        </input>
-        <input
-          type="text"
-          value = {date}
-          onChange = {(e)=> (setDate(e.target.value))}
-          placeholder = "YYYY-MM-DD"
-          >
-        </input>
-        <button onClick={fetchPolygonAPI}>Find Stock</button> 
-      </div>
+    <div className="App"> 
+        <div className = "title">Stocks Daily: Open to Close</div>
+        <div className = "intro">Add your ticker and the date using YYYY-MM-DD formate to find your stocks data. The 
+          search will return the opening, closing, high, low, pre market, after hours, and price and percentage changes from the date
+          entered. Results are adjusted for splits. </div>
+        <div className = "inputsButton">
+            <input
+              type = "text"
+              value = {ticker}
+              onChange={(e)=> setTicker(e.target.value)}
+              placeholder = "ticker"
+              >
+            </input>
+            <input
+              type="text"
+              value = {date}
+              onChange = {(e)=> (setDate(e.target.value))}
+              placeholder = "YYYY-MM-DD"
+              >
+            </input>
+            <button onClick={fetchPolygonAPI}>Find Stock</button> 
+        </div>  
     </div>
+
     { stock.symbol ?
-    <div className = "returned Info">
-      
-      <div>Ticker: {stock.symbol}</div>
-      <div>Date Searched: {date}</div>
-      <div>Low: {stock.low}</div>
-      <div>High: {stock.high}</div>
-      <div>Open: {stock.open}</div>
-      <div>Close: {stock.close}</div>
-      <div> Percentage change from open to close: </div>
-      <div> Percentage changed today: </div>
+      <div className = "returned Info">
+        <div className = "tickerName">Ticker: {stock.symbol}</div>
+            <div className = "tickerData" >
+                <div>Date Searched: {stock.from}</div>
+                <div>Low: {stock.low}</div>
+                <div>High: {stock.high}</div>
+                <div>Open: {open}</div>
+                <div>Close: {close}</div>
+                <div> Price change from open to close: {difference}</div>
+                <div> Grapper: {percentChange} % </div>
+                <div>Pre Market: {stock.preMarket}</div>
+                <div>After Hours:{stock.afterHours}</div>
+            </div>
+      </div> 
+    : ""}
 
-
-
-    </div> : ""}
-    {stock.status ? <div> We can't find this stock on this date. Please check inputs and try again</div> : ""}
+    {stock.status === 'NOT_FOUND' ? 
+        <div className = "notfound"> We can't find this stock on this date. Please check inputs and try again</div> 
+    : ""}
     
-   
-   
-    </>
+   </>
   );
 }
 
